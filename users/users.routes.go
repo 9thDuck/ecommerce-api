5f852d4dbefd7bb4a8915f5786ebad20ace5df03
Mylavarapu Sarath Sai <msai@live.co.uk>
2024-09-14 10:17:57 +0530

    added token verification for getUserById

--- users/users.routes.go
+++ users/users.routes.go
@@ -1,11 +1,12 @@
 package users
 
 import (
+	"github.com/9thDuck/ecommerce-api.git/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterRoutes(server *fiber.App) {
 	server.Post("/users/signup", signup)
 	server.Post("/users/login", login)
-	server.Get("/users/:id", getUserById)
+	server.Get("/users/:id", middleware.VerifyToken, getUserById)
 }
